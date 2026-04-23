94be78198efd2f2f70d7584c776d879d014dc91d
Anshul Dhamija <anshul.dhamija@outlook.com>
2026-04-23 20:50:47 +0530

    feat: init project structure and install dependencies

---
+++ constants/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+  primary: '#1E90FF',
+  secondary: '#FF6347',
+  background: '#F5F5F5',
+  text: '#333333',
+  white: '#FFFFFF',
+  black: '#000000',
+};
---
+++ constants/strings.js
@@ -0,0 +1,4 @@
+export const strings = {
+  appName: 'FindMyProf',
+  welcomeMessage: 'Welcome to FindMyProf',
+};
